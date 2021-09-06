79278fd6b39ad7b93a3fc6042d89307515edd5ee
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2021-09-06 12:33:27 +0200

    bugfix

--- src/Core/TextBased.php
+++ src/Core/TextBased.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ . '/../../../../vendor/autoload.php';
+include_once __DIR__ . '/../../../../autoload.php';
 if (is_file("./Core/CLI/initCLI.php")) {
     include_once "./Core/CLI/initCLI.php";
 }
